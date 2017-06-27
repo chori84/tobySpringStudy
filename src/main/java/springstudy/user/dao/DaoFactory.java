35c517f95f9ffbc93260341892c51d55e8259076
HeeChulChoi <slivers721@gmail.com>
2017-06-28 00:49:29 +0900

    1.4 제어의 역전    1.4.2 오브젝트 팩토리의 활용
    - 중복 소스를 메소드로 만듬.

--- src/main/java/springstudy/user/dao/DaoFactory.java
+++ src/main/java/springstudy/user/dao/DaoFactory.java
@@ -6,9 +6,17 @@ package springstudy.user.dao;
 public class DaoFactory {
     public UserDao userDao() {
         // UserDao 타입의 오브젝트를 어떻게 만들고, 어떻게 준비시킬지 결정
-        ConnectionMaker connectionMaker = new MyConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        UserDao userDao = new UserDao(connectionMaker());
 
         return userDao;
+    }
+
+//    public AccountDao accountDao() {
+//        return new AccountDao(connectionMaker());
+//    }
+
+    // 여러 DAO에서 사용할 수 있기 때문에 중복을 피하기 위해 메소드로 빼놓음.
+    public ConnectionMaker connectionMaker() {
+        return new MyConnectionMaker();
     }
 }
