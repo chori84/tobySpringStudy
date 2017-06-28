28d5ecc84a414b5343da97ee1fc5801b64bd7dfa
HeeChulChoi <slivers721@gmail.com>
2017-06-28 23:49:36 +0900

    1.8 XML을 이용한 설정   1.8.2 XML을 이용하는 애플리케이션 컨텍스트
    - DaoFactory 대신 XML 설정 정보를 이용.

--- src/main/java/springstudy/user/dao/UserDaoTest.java
+++ src/main/java/springstudy/user/dao/UserDaoTest.java
@@ -2,6 +2,7 @@ package springstudy.user.dao;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springstudy.user.domain.User;
 
 import java.sql.SQLException;
@@ -11,7 +12,7 @@ import java.sql.SQLException;
  */
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         // 테스트 클래스의 목적인 UserDao의 테스트 역할만을 위해 Connection 객체 생성을 DaoFactory에게 넘겼다.
         UserDao dao = context.getBean("userDao", UserDao.class);
 
