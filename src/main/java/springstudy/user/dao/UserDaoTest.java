9d7360ba9a891e682a73a123c633e9145d7159a5
HeeChulChoi <slivers721@gmail.com>
2017-07-02 13:35:26 +0900

    2장 테스트   2.2 UserDaoTest 개선     2.2.2 테스트의 효율적인 수행과 결과 관리
    - JUnit 테스트로 전환
    - junit 4.12로 변경

--- src/main/java/springstudy/user/dao/UserDaoTest.java
+++ src/main/java/springstudy/user/dao/UserDaoTest.java
@@ -1,5 +1,8 @@
 package springstudy.user.dao;
 
+import org.junit.Test;
+
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -7,28 +10,33 @@ import springstudy.user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by chori on 2017. 6. 28..
  */
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    @Test
+    public void addAndGet() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         // 테스트 클래스의 목적인 UserDao의 테스트 역할만을 위해 Connection 객체 생성을 DaoFactory에게 넘겼다.
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
-
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+    }
+
+    public static void main(String[] args) {
+        JUnitCore.main("springstudy.user.dao.UserDaoTest");
     }
 }
