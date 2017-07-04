2da3e26a6f8ea6812996cdf74c4e94d37a150292
HeeChulChoi <slivers721@gmail.com>
2017-07-04 23:51:39 +0900

    2.4 스프링 테스트 적용   2.4.1 테스트를 위한 애플리케이션 컨텍스트 관리
    - 스프링 테스트 컨텍스트 프레임워크 적용
    - spring-test dependency 추가
    - 테스트에 스프링 테스트 적용
    - 애플리케이션 컨텍스트 생성을 위한 @ContextConfiguration 사용
    - @Autowired를 이용한 자동 DI

--- src/test/java/springstudy/user/dao/UserDaoTest.java
+++ src/test/java/springstudy/user/dao/UserDaoTest.java
@@ -4,10 +4,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springstudy.user.domain.User;
 
 import java.sql.SQLException;
@@ -18,17 +22,21 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by chori on 2017. 6. 28..
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
     private UserDao dao;
+
     private User user1;
     private User user2;
     private User user3;
 
     @Before
     public void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        dao = context.getBean("userDao", UserDao.class);
-
         user1 = new User("gyumee", "박성철", "springno1");
         user2 = new User("leegw700", "이길원", "springno2");
         user3 = new User("bumjin", "박범진", "springno3");
