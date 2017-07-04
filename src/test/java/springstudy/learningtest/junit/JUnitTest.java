40099ec631871d385f7300c14eafb9c24a6a6ea3
HeeChulChoi <slivers721@gmail.com>
2017-07-05 00:46:31 +0900

    2.5 학습 테스트로 배우는 스프링   2.5.2 학습 테스트 예제
    - 스프링 테스트 컨텍스트 테스트
    - JUnitTest 위한 junit.xml 컨텍스트 생성
    - JUnitTest에 스프링 테스트 적용
    - 컨텍스트 테스트 추가

--- src/test/java/springstudy/learningtest/junit/JUnitTest.java
+++ src/test/java/springstudy/learningtest/junit/JUnitTest.java
@@ -1,55 +1,57 @@
 package springstudy.learningtest.junit;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.matchers.JUnitMatchers.either;
 import static org.junit.matchers.JUnitMatchers.hasItem;
 
 /**
  * Created by chori on 2017. 7. 5..
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:junit.xml")
 public class JUnitTest {
-    static JUnitTest testObject;
+    @Autowired
+    ApplicationContext context;
+
     static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
-    }
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
 
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
+    }
     @Test
     public void test2() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
-    }
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
 
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
+    }
     @Test
     public void test3() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
-    }
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
 
-    @Test
-    public void test4() {
-        assertThat(testObjects, not(hasItem(this)));
-        testObjects.add(this);
-    }
-    @Test
-    public void test5() {
-        assertThat(testObjects, not(hasItem(this)));
-        testObjects.add(this);
-    }
-    @Test
-    public void test6() {
-        assertThat(testObjects, not(hasItem(this)));
-        testObjects.add(this);
+        assertThat(contextObject, either(is(nullValue())).or(CoreMatchers.<Object>is(this.context)));
+        contextObject = this.context;
     }
 
 
